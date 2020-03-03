04edc43c2d52d4aa8fbdb8160b52b3355a042c74
ludovic <ludovic.pecqueur@college-de-france.fr>
2020-03-03 15:21:13 +0100

    Zstack merging within GUI or command line

--- bin/AMI_Image_Analysis.sh
+++ bin/AMI_Image_Analysis.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 #File generated with Setup_VENV.py
-virtenv="/usr/local/PROGRAMS/XTAL/GIT-AMi_Image_Analysis/python/venvs/AMI_IMAGE_ANALYSIS_TENSORFLOW1/bin"
-. ${virtenv}/activate
+#virtenv="/usr/local/PROGRAMS/XTAL/GIT-AMi_Image_Analysis/python/venvs/AMI_IMAGE_ANALYSIS_TENSORFLOW1/bin"
+#. ${virtenv}/activate
 
 #DO NOT EDIT the next THREE LINES
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
